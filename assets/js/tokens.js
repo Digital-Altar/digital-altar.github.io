a2a4f3c27ee993b1d35d1fb4168cc84785e81711
digitalaltar <moon@digitalaltar.com>
2023-03-25 14:54:51 -0700

    Adding Tezos and Ethereum gallery support

--- assets/js/tokens.js
+++ assets/js/tokens.js
@@ -1,6 +1,6 @@
-function loadTokens() {
+function tezosTokens() {
   // Loop through each token and output a div for each one
-  tokens.forEach(token => {
+  tezos.forEach(token => {
     // Query the API to get token data
     axios.get(`https://api.tzkt.io/v1/tokens?contract=${token.address}&offset=${token.tokenId}&limit=1`)
       .then(response => {
@@ -40,6 +40,43 @@ function loadTokens() {
   });
 }
 
+function ethTokens() {
+  // Loop through each token and output a div for each one
+  eth.forEach(token => {
+    // Query the LooksRare API to get token data
+    axios.get(`https://api.looksrare.org/api/v1/tokens?collection=${token.address}&tokenId=${token.tokenId}`)
+      .then(response => {
+        // Get data for the token
+        const tokenData = response.data;
+        console.log(tokenData);
+
+        const name = tokenData.data.name;
+        console.log(name); // CryptoKitties #3234090
+
+        const image = tokenData.data.imageURI;
+        console.log(image);
+
+        // Create HTML for image and name
+        const html = `
+          <div class="nft ethereum">
+            <a href="https://looksrare.org/collections/${token.address}/${token.tokenId}">
+              <img src="${image}" alt="${name}" />
+              <p><span>${name}</span></p>
+            </a>
+          </div>
+        `;
+
+        // Add HTML to token-data div
+        const tokenDataDiv = document.getElementById('collection');
+        tokenDataDiv.innerHTML += html;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
-  loadTokens();
+  tezosTokens();
+  ethTokens();
 });
